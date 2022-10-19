b27bd9897f4ab6f4123ab925eae9f3ad7e7618c6
Ashley Towey <a.towey0220201@arts.ac.uk>
2022-10-19 11:08:04 +0100

    failed to create a counter that will communicate with p5

--- 5-arduino-to-p5js/P5JS/p5-serial-incoming/sketch.js
+++ 5-arduino-to-p5js/P5JS/p5-serial-incoming/sketch.js
@@ -1,7 +1,8 @@
 let port;
 let connectBtn;
-let preVal = 0;
-let btnPreVal = 0;
+let preVal = 0; // declare potentiometer base value to stop the sketch flickering
+let btnPreVal = 0; // declare button base value to stop the sketch flickering
+let btnClicks = 0;
 
 function setup() {
   createCanvas(600, 600);
@@ -9,6 +10,7 @@ function setup() {
 
   port = createSerial();
 
+  // initial setup button
   connectBtn = createButton('Connect to Arduino');
   connectBtn.position(20, 20);
   connectBtn.mousePressed(connectBtnClick);
@@ -20,36 +22,41 @@ function draw() {
 
   // reads in complete lines and prints them at the
   // bottom of the canvas
-  let inputString = port.readUntil("\n")/* || preVal*/;
-  let val = inputString.split(':')[0] || preVal;
-  //let val = serialRead.split
-  
+  let inputString = port.readUntil("\n"); // declare input variable as the arduino serial output
+
+  // potentiometer input
+  let val = inputString.split(':')[0] || preVal; // splitting the input string and extracting the potentiometer value
+  // change the value of preVal if val is communicated
     if (val) {
       preVal = val;
     }
 
-    let btnVal = inputString.split(':')[1] || btnPreVal;
+    // button input
+    let btnVal = inputString.split(':')[1] || btnPreVal; // splitting the input string and extracting the button value
+    // change the value of preBtnVal if  btnVal is received
     if (btnVal) {
       btnPreVal = btnVal;
     }
 
     // print out the value of serial or val to the console
-    console.log(val, btnVal);
+    console.log(val, ' & ', btnVal, '+', btnClicks);
 
+  /* commenting out to test rotating between shapes
   if (val.length > 0) {
     //display the incoming data
     fill(0);
-    text(val, 10, height-20);
+    text(val + ' & ' + btnVal, 10, height-20);
     
     //do something with the data!
     noStroke();
     fill(255,200,0);
     ellipse(width/2,height/2,val,val);
-    
   }
+*/
 
-  if (btnVal> .5) {
-    rect (50, 50, 200);
+  // when the button is pressed change the state of shape
+  if (btnVal > .5) {
+    changeShape();
   }
 
   // changes button label based on connection status
@@ -68,3 +75,11 @@ function connectBtnClick() {
   }
 }
 
+// function to rotate between three different shapes & called when the button is pressed
+function changeShape() {
+  // for (let i = 0; i > 2; i++){
+  //   btnClicks + i;
+    // text(val + ' & ' + btnVal + '+' + btnClicks, 10, height-20);
+  //}
+  btnClicks + 1
+}
